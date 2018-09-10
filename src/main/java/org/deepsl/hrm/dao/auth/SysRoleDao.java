9de7ad4d8f2f6a92ab5629a0036bcfb380227aba
kingrowing <783489027@qq.com>
2018-09-10 23:19:21 +0800

    "add project hrm_ssm"

---
+++ src/main/java/org/deepsl/hrm/dao/auth/SysRoleDao.java
@@ -0,0 +1,12 @@
+package org.deepsl.hrm.dao.auth;
+
+
+
+import org.deepsl.hrm.domain.auth.SysRole;
+
+import java.util.List;
+
+public interface SysRoleDao {
+
+    List<SysRole> findSysRoleByUid(Integer uid);
+}
